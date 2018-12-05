ab9e0740f8cdc46da78f634a472ce8f9e4c832ef
Julian M. Qapo <julian.qapo@gmail.com>
2018-12-05 12:41:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+let driver = {};
+
+
+function updateDriverWithKeyAndValue(sam, address, Broadway){
+  driver["name"] = sam;
+  driver["address"] = Broadway;
+  return driver
+}
